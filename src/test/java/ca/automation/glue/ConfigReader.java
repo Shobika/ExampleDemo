4bf8f08bcec7efbcfa0f9d7daf80ec11d039135d
Shobika <shobikaksr@gmail.com>
2019-12-09 13:32:32 -0500

    added code to override cucumber options from command line

--- src/test/java/ca/automation/glue/ConfigReader.java
+++ src/test/java/ca/automation/glue/ConfigReader.java
@@ -14,6 +14,7 @@ public class ConfigReader {
     public static String getPropertyvalue(String property) throws IOException {
         Properties prop = new Properties();
         prop.load(new FileInputStream(System.getProperty("user.dir")+"/src/test/resources/cucumber.properties"));
+
         return prop.getProperty(property);
     }
 }
